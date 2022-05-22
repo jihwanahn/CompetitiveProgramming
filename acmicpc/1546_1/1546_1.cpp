0a9e4ac28e82ad77c451576dd2a7fc31b24bf183
Jihwan Peter Ahn <33449025+jihwanahn@users.noreply.github.com>
2022-05-22 22:24:06 +0900

    solved 1546

---
+++ acmicpc/1546_1/1546_1.cpp
@@ -0,0 +1,42 @@
+// 1546_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+auto main() -> int
+{
+	int N;
+	
+	cin >> N;
+
+	vector < double > scores(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> scores.at(i);
+	}
+
+	sort(scores.begin(), scores.end());
+	double sum = 0;
+	for (auto i = 0; i < N; i++)
+	{
+		sum += (scores.at(i) / scores.at(N-1))* 100;
+	}
+	
+	cout << sum / N;
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
