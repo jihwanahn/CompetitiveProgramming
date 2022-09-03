66a6acd62fed8120939257ef1b2fd350f4ad9e8e
Jihwan Peter Ahn <33449025+jihwanahn@users.noreply.github.com>
2022-09-03 23:33:52 +0900

    [Bronze II] Title: OX퀴즈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int OX(string& s) {
+	
+	int cumulative = 0;
+	int sum = 0;
+ 
+	for(char &v : s) {
+		if(v == 'O') {
+			cumulative++;
+			sum += cumulative;
+		}
+		else {
+			cumulative = 0;
+		}
+	}
+	return sum;
+}
+ 
+int main(int argc, const char * argv[]) {
+ 
+	ios_base::sync_with_stdio(0);
+	cin.tie(nullptr);
+ 
+	int T;
+	cin >> T;
+ 
+	for(int i = 0; i < T; i++) {
+		string s;
+		cin >> s;
+ 
+		cout << OX(s) << "\n";
+	}
+ 
+	return 0;
+}
