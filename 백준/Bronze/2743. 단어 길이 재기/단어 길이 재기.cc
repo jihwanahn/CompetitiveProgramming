85eeafb0d2cd4deaee356fb6dd1bf767750058b9
Jihwan Peter Ahn <33449025+jihwanahn@users.noreply.github.com>
2022-09-09 00:57:40 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+  cin.tie(NULL);
+  cin.sync_with_stdio(0);
+
+  string a;
+  cin >> a;
+  cout << a.size();
+  
+	return 0;
+}
