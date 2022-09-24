f8e54a94f0ddfa74af0f4dfebf748466c9a14d7f
Jihwan Peter Ahn <33449025+jihwanahn@users.noreply.github.com>
2022-09-25 01:14:02 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    double sum = 0;
+    for (auto i : arr)
+    {
+        sum += i;
+    }
+    answer = sum / arr.size();
+    return answer;
+}
